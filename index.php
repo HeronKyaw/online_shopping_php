e4480150b77d695ef3115673e748b72d2285500a
Wunna Kyaw <heronskyaw@gmail.com>
2023-07-16 17:17:15 +0630

    16th July 2023

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+include("admin/confs/config.php");
+
+$cart = 0;
+
+if (isset($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $qty) {
+        $cart += $qty;
+    }
+}
+
+#Browse by Category
+if (isset($_GET['cat'])) {
+    $cat_id = $_GET['cat'];
+    $items = mysqli_query($conn, "SELECT * FROM tbl_item WHERE category_id = $cat_id");
+} else {
+    $items = mysqli_query($conn, "SELECT * FROM tbl_item");
+}
+
+#search result
+if (isset($_GET['q'])) {
+    // $items = search_perform($_GET['q'], "items", "title");
+}
+$cats = mysqli_query($conn, "SELECT * FROM tbl_category");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Shop</title>
+</head>
+
+<body>
+    <h1 id="title" style="font-family: 'Courier New', Courier, monospace;">Online Shop</h1>
+    <div class="admininfo">
+        <a href="admin/auth/login.php" style="font-family:'Courier New', Courier, monospace;">Admin Login</a>
+    </div>
+    <div class="info">
+        <a href="view-cart.php">
+            (<?php echo $cart ?>) items in your cart
+        </a>
+    </div>
+    <div class="sidebar">
+        <ul class="cats">
+            <li>
+                <b><a href="index.php">All Items</a></b>
+            </li>
+            <?php while ($row = mysqli_fetch_assoc($cats)) : ?>
+                <li>
+                    <a href="index.php?cat=<?php echo $row['id'] ?>">
+                        <?php echo $row['name'] ?>
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
+
+    <div class="main">
+        <ul class="items">
+            <?php while ($row = mysqli_fetch_assoc($items)) : ?>
+                <li>
+                    <?php !is_dir("admin/image/items/{$row['photo']}") and file_exists("admin/image/items/{$row['photo']}") ?>
+                    <img src="admin/image/items/<?php echo $row['photo']?>" alt="<?php echo $row['title'] ?>'s image" height="150">
+                    <b>
+                        <a href="item_detail.php?id=<?php echo $row['id'] ?>">
+                            <?php echo $row['title'] ?>
+                        </a>
+                    </b>
+                    <i>$<?php echo $row['price'] ?></i>
+                    <a href="add-to-cart.php?id=<?php echo $row['id'] ?>" class="add-to-cart">
+                        Add to Cart
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    </div>
+    <div class="footer">
+        &copy; <?php echo date("Y") ?>. All right reserved.
+    </div>
+</body>
+
+</html>
---
+++ item_detail.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Detail</title>
+</head>
+<body>
+    <h1>Item Details</h1>
+    <?php
+        include("admin/confs/config.php");
+        $id = $_GET['id'];
+        $item = mysqli_query($conn, "SELECT * FROM tbl_item WHERE id = $id");
+        $row = mysqli_fetch_assoc($item);
+    ?>
+    <div class="detail">
+        <a href="index.php" class="back">Back</a>
+        <img src="admin/image/items/<?php echo $row['photo']?>" alt="<?php echo $row['title']?>'s Image" width="300" height="200">
+        <h2><?php echo $row['title']?></h2>
+        <i>by <?php echo $row['brand']?></i>
+        <b>$<?php echo $row['price']?></b>
+        <p><?php echo $row['review']?></p>
+        <a href="add_to_cart.php?id=<?php echo $row['id']?>" class="add-to-cart">
+            Add to Cart
+        </a>
+    </div>
+    <div class="footer">
+        &copy; <?php echo date("Y") ?>. All right reserved.
+    </div>
+</body>
+</html>
