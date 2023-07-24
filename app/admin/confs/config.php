496438030766195268ac29bc201d7413620a0791
wunnakyaw <>
2023-07-24 23:37:23 +0630

    added mysql and nginx to docker

--- app/admin/confs/config.php
+++ app/admin/confs/config.php
@@ -1,7 +1,7 @@
 <?php
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
+    $dbhost = "127.0.0.1";
+    $dbuser = "os_admin";
+    $dbpass = "admin@os2221";
     $dbname = "db_online_shopping";
     $conn = mysqLi_connect($dbhost, $dbuser, $dbpass);
     mysqli_select_db($conn, $dbname);
